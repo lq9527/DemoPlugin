27615aa0ab19d08c51ee640867a68c8403b459af
liqiang32 <liqiang32@58ganji.com>
2019-10-15 17:41:03 +0800

    init

---
+++ app/src/main/java/com/lq/publishplugin/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lq.publishplugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lq.lqplugin.LqPlugin;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LqPlugin.hello();
+
+    }
+}
